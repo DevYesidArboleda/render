156323568c8ca5d003e7c796425fb11549bc4964
crisye banguera <yesison714@gmail.com>
2023-06-23 13:19:34 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import app from './app.js'
 import { connectDB } from './db.js'
 
-connectDB();
-app.listen(9000)
+const port = process.env.PORT || 9000;
 
-console.log("El servidor esta en el puerto ", 9000)
+connectDB();
+app.listen(port)
+
+console.log("El servidor esta en el puerto ", port)
